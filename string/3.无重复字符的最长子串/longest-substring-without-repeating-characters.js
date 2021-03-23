c2ae7526dcbd9d7c7344ff91450156f1f324bb30
custs21113 <mixjay@outlook.com>
2021-03-24 02:18:26 +0800

    3.longest-substring-without-repeating-characters

---
+++ string/3.无重复字符的最长子串/longest-substring-without-repeating-characters.js
@@ -0,0 +1,14 @@
+var lengthOfLongestSubstring = function(s) {
+    let map = new Map();
+    let index =0;
+    let n = s.length;
+    for(let start = 0, end = 0; end < n; end ++){
+        let ch = s[end]
+        if(map.has(ch)){
+            start = Math.max(map.get(ch),start)
+        }
+        index = Math.max(index, end - start +1)
+        map.set(ch, end + 1)
+    }
+    return index
+};
---
+++ string/3.无重复字符的最长子串/longest-substring-without-repeating-characters.ts
@@ -0,0 +1,14 @@
+function lengthOfLongestSubstring(s:string):number {
+    let map = new Map();
+    let index:number = 0;
+    let n:number = s.length;
+    for(let start:number = 0, end:number = 0; end < n; end ++){
+        let ch:string = s[end]
+        if(map.has(ch)){
+            start = Math.max(map.get(ch),start)
+        }
+        index = Math.max(index, end - start +1)
+        map.set(ch, end + 1)
+    }
+    return index
+};
