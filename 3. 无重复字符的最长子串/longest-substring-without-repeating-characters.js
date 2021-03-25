8608c244c90a9919cd113ad8d2fbe27051c95502
custs21113 <mixjay@outlook.com>
2021-03-26 01:51:29 +0800

    3. 无重复字符的最长子串/longest-substring-without-repeating-characters

--- 3. 无重复字符的最长子串/longest-substring-without-repeating-characters.js
+++ 3. 无重复字符的最长子串/longest-substring-without-repeating-characters.js
@@ -2,17 +2,17 @@
  * @param {string} s
  * @return {number}
  */
- var lengthOfLongestSubstring = function(s) {
+var lengthOfLongestSubstring = function (s) {
     let map = new Map();
-    let index =0;
+    let index = 0;
     let n = s.length;
-    for(let start = 0, end = 0; end < n; end ++){
+    for (let start = 0, end = 0; end < n; end++) {
         let ch = s[end]
-        if(map.has(ch)){
-            start = Math.max(map.get(ch),start)
+        if (map.has(ch)) {
+            start = Math.max(map.get(ch), start);
         }
-        index = Math.max(index, end - start +1)
-        map.set(ch, end + 1)
+        index = Math.max(index, end - start + 1);
+        map.set(ch, end + 1);
     }
-    return index
+    return index;
 };
--- 3. 无重复字符的最长子串/longest-substring-without-repeating-characters.ts
+++ 3. 无重复字符的最长子串/longest-substring-without-repeating-characters.ts
@@ -2,17 +2,17 @@
  * @param {string} s
  * @return {number}
  */
- var lengthOfLongestSubstring = function(s:string):number {
+var lengthOfLongestSubstring = function (s: string): number {
     let map: Map<string, number> = new Map();
-    let index: number =0;
+    let index: number = 0;
     let n: number = s.length;
-    for(let start: number = 0, end: number = 0; end < n; end ++){
-        let ch: string = s[end]
-        if(map.has(ch)){
-            start = Math.max(map.get(ch),start)
+    for (let start: number = 0, end: number = 0; end < n; end++) {
+        let ch: string = s[end];
+        if (map.has(ch)) {
+            start = Math.max(map.get(ch), start);
         }
-        index = Math.max(index, end - start +1)
-        map.set(ch, end + 1)
+        index = Math.max(index, end - start + 1);
+        map.set(ch, end + 1);
     }
-    return index
+    return index;
 };
