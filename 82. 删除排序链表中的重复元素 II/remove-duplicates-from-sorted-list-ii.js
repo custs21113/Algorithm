3e3100b91b50aed77e3fdb1cb922297dda8f3540
custs21113 <mixjay@outlook.com>
2021-03-27 14:07:13 +0800

    82. 删除排序链表中的重复元素 II/remove-duplicates-from-sorted-list-ii

---
+++ 82. 删除排序链表中的重复元素 II/remove-duplicates-from-sorted-list-ii.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+    if (head === null || head.next === null) {
+        return head;
+    }
+    if (head.val === head.next.val) {
+        while (head.next && head.next.val === head.val) {
+            head.next = head.next.next;
+        }
+        return deleteDuplicates(head.next);
+    } else {
+
+        head.next = deleteDuplicates(head.next);
+    }
+    return head;
+};
